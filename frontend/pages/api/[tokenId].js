9e96488cd9f8e90526796dbae9a97062061bd1ab
raghunath18m <raghunath18m@gmail.com>
2022-06-26 18:36:26 +0530

    added nft images

--- frontend/pages/api/[tokenId].js
+++ frontend/pages/api/[tokenId].js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     const image_url = "";
     res.status(200).json({
-      name: "MUN" + tokenId,
+      name: "United Legend #" + tokenId,
       description: "MUN is collection of NFTs representing the famous players played at Manchester United FC",
-      image: image_url + tokenId + ".svg",
+      image: image_url + tokenId + ".png",
     });
   }
